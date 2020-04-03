c0be380d4eea16dd28899a6c008bc50d0fdeed13
Serginho <serginhotec@gmail.com>
2020-04-03 19:39:39 -0300

    eita...

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://meu-back-end.herokuapp.com/ongs'
+    baseURL: 'https://meu-back-end.herokuapp.com'
 });
 
 export default api;
